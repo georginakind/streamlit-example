a81af2696d4f577ef5efd8f8725f9f48956a1253
georginakind <101234149+georginakind@users.noreply.github.com>
2023-08-27 18:33:36 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ happy birthday!
 this is your own personal drinking game with meow song geneorator included! Straight out of HITS N WIGS 
 """
 
+url = "https://raw.githubusercontent.com/georginakind/streamlit-example/master/hits_n_wigs.csv"
+c=pd.read_csv(url)
 
 #st.write(df)
 #st.write("data below")
