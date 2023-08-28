8db8bad9dce9541c29ed5f8ae6c40946836e3c33
georginakind <101234149+georginakind@users.noreply.github.com>
2023-08-28 20:49:01 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -246,10 +246,10 @@ if st.button('LETS GO GIRLS'):
     st.write("You have 5 more seconds")
     sleep(5)
     if trivia_choice == "Movies":
-        m_question, m_answer = random.choice(list(movies_dict.items()))
-        st.write(m_question)
-        sleep(5)
-        st.write(m_answer)      
+      m_question, m_answer = random.choice(list(movies_dict.items()))
+      st.write(m_question)
+      sleep(5)
+      st.write(m_answer)      
 
     elif trivia_choice == "Taylor Swift":
         t_question, t_answer = random.choice(list(taylor_dict.items()))
@@ -257,7 +257,8 @@ if st.button('LETS GO GIRLS'):
         sleep(5)
         st.write(t_answer)
 
-    else: st.write(trivia_choice)
+    else: 
+        st.write(trivia_choice)
     
   elif decision == "action":
     action = random.choice(action_options)
