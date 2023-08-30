e144e0f3c912546e7d5b68e0c676245fd82db266
georginakind <101234149+georginakind@users.noreply.github.com>
2023-08-30 16:28:46 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,7 +256,7 @@ st.write(create_player_list)
 #df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 #st.write(df.head(5))
 
-decision_options = ["sports", "general knowledge', "movie", "taylor swift", "action", "action", "drink", "meow", "meow", "meow", "meow", "song game", "song game", "song game"]
+decision_options = ["sports", "general knowledge", "movie", "taylor swift", "action", "action", "drink", "meow", "meow", "meow", "meow", "song game", "song game", "song game"]
 action_options = ["last person to hit the deck drinks", "last person with their hands up drinks", "girls drink", "boys drink", "they drink (we all about equality in this game)", "least drunk drink", "last person to touch a dog drinks", "last person to touch a wall drinks", "last person to stand up drinks"]
 
 
